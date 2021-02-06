03888141167113e92dd62c784e8f7f287549ced7
vineeth357 <vineethvinnu357@gmail.com>
2021-02-06 23:02:28 +0530

    adding files

---
+++ admins.js
@@ -0,0 +1,27 @@
+const adminUser = require('./models/adminUser');
+const mongoose = require('mongoose');
+
+var adminuser = new adminUser({
+    email: 'vineethvinnu357@gmail.com',
+    password: 'qwert123',
+    role: 'admin'
+});
+
+const email = "pattigar@iitg.ac.in";
+const password = "qwert123";
+const role = "restricted";
+
+const dbURI = 'mongodb+srv://vineeth:qwert123@test.h1ng1.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then(async (result) => {
+    console.log('connection made!!');
+    adminuser.save().then(function(){
+        console.log("admin created suceesfully");
+    });
+    const adminuser_ = await adminUser.create({ email, password, role });
+    console.log(adminuser_);
+    })
+  .catch((err) => console.log(err));
+
+
+
